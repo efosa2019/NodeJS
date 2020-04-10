410914df6deae5afd42cea90eb310b63e0008b41
efosa2019 <efosa.alohan@live.com>
2020-04-10 03:02:46 +0200

    Returning a result of an array of 1000 items.

---
+++ read-file.js
@@ -0,0 +1,14 @@
+const csvjson = require('csvjson');
+const readFile = require('fs').readFile;
+readFile('./customer-data.csv', 'utf-8', (err, fileContent) => {
+	if (err) {
+		console.log(err); // Do something to handle the error or just throw it
+		throw new Error(err);
+	}
+	const jsonObj = csvjson.toObject(fileContent);
+
+	// iterates over array elements
+	for (let result of jsonObj) {
+		console.log(result);
+	}
+});
---
+++ read-stream.js
@@ -0,0 +1,9 @@
+const createReadStream = require('fs').createReadStream;
+const createWriteStream = require('fs').createWriteStream;
+const csvjson = require('csvjson');
+const toObject = csvjson.stream.toObject();
+const stringify = csvjson.stream.stringify();
+createReadStream('./customer-data.csv', 'utf-8')
+	.pipe(toObject)
+	.pipe(stringify)
+	.pipe(createWriteStream('./customer-data.json'));
